6dfcaf75cdbe09512d3f022678183499fab322e1
Miguel Toribio <miguel.toribio@b89.io>
2025-05-23 20:26:53 -0500

    Initial commit

---
+++ test/softtek.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Softtek from '../lib/softtek-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/softtek-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Softtek.SofttekStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
